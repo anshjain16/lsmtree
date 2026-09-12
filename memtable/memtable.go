1f0593a0f2f98650f676d79fed1f0f5522dc0f10
anshjain16 <anshjain0216@gmail.com>
2026-09-12 16:24:36 +0530

    memtable: Add memtable implementation
    - adds functionalities such as create, insert, delete, search
    - TODO: implement proper test for avl tree
    - TODO: implement proper error handling for whole memtable package

--- memtable/memtable.go
+++ memtable/memtable.go
@@ -3,20 +3,52 @@ package memtable
 import (
 )
 
-// Create a memtable
+type Memtable struct {
+	root *Node
+	threshold int
+}
 
+// Create a memtable
+func Create(threshold int) *Memtable {
+	return &Memtable{
+		root: nil,
+		threshold: threshold,
+	}
+}
 
 
 
 // Insert a key value pair in memtable
-
+func Insert(memtable *Memtable, key string, value string) *Memtable{
+	updatedMemtableRoot := AvlInsert(memtable.root, key, value)
+	memtable.root = updatedMemtableRoot
+	return memtable
+}
 
 
 
 // Search a key in memtable
+func Search(memtable *Memtable, key string) (string, string){
 
+	isPresent, value := AvlSearchKey(memtable.root, key)
 
+	if isPresent {
+		return key, value
+	}
 
+	return key, "KEY_NOT_PRESENT"
+
+}
+
+// Delete a key
+func Delete(memtable *Memtable, key string) *Memtable{
+
+	updatedMemtableRoot := AvlInsert(memtable.root, key, "TOMBSTONED")
+	memtable.root = updatedMemtableRoot
+
+	return memtable
+
+}
 
 
 // Flush the memtable to persistent storage (SSTABLE)
